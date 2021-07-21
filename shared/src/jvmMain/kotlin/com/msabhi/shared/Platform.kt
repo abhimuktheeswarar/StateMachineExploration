517238447917b2821ccdb441e31b2be7af0d125a
Abhi <abeemukthees@gmail.com>
2021-07-21 12:34:08 +0530

    Initial commit

---
+++ shared/src/jvmMain/kotlin/com/msabhi/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.msabhi.shared
+
+actual class Platform actual constructor() {
+    actual val platform: String = "JVM"
+}
---
+++ shared/src/jvmTest/kotlin/com/msabhi/shared/jvmTest.kt
@@ -0,0 +1,12 @@
+package com.msabhi.shared
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class JvmGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue(Greeting().greeting().contains("JVM"), "Check JVM is mentioned")
+    }
+}
